b6fd1f885e3a177b9d5230a14d8d8dfbfa57b5a8
Andrew Steinmetz <31364841+plusmobileapps@users.noreply.github.com>
2021-10-15 23:39:01 -0700

    add a mock network test helper to clean up some boiler plate from the test (#11)

--- app/src/androidTest/java/com/plusmobileapps/kotlinopenespresso/util/NetworkTestHelper.kt
+++ app/src/androidTest/java/com/plusmobileapps/kotlinopenespresso/util/NetworkTestHelper.kt
@@ -1,18 +1,49 @@
 package com.plusmobileapps.kotlinopenespresso.util
 
+import com.plusmobileapps.kotlinopenespresso.data.LoginDataSource
+import com.plusmobileapps.kotlinopenespresso.data.model.LoginResponse
 import io.ktor.client.*
 import io.ktor.client.engine.mock.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
 import io.ktor.http.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
-fun mockHttpClient(handler: MockRequestHandler): HttpClient {
-    return HttpClient(MockEngine) {
+class MockNetworkTestHelper {
+
+    val client = HttpClient(MockEngine) {
         install(JsonFeature) {
             serializer = KotlinxSerializer()
         }
         engine {
-            addHandler(handler)
+            addHandler { request: HttpRequestData ->
+                when (request.url.fullUrl) {
+                    LoginDataSource.LOGIN_URL -> getLoginResponse.invoke(this, request)
+                    else -> error("Unhandled ${request.url.fullUrl}")
+                }
+            }
+        }
+    }
+
+    private var getLoginResponse: MockRequestHandler = defaultLoginResponseHandler
+
+    fun everyLoginReturns(response: MockRequestHandler) {
+        this.getLoginResponse = response
+    }
+
+    fun destroy() {
+        getLoginResponse = defaultLoginResponseHandler
+    }
+
+    companion object {
+        val defaultLoginResponseHandler: MockRequestHandler = {
+            respond(
+                Json.encodeToString(LoginResponse("default-id")),
+                HttpStatusCode.OK,
+                headersOf("Content-Type", ContentType.Application.Json.toString())
+            )
         }
     }
 }
